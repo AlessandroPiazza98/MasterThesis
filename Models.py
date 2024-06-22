d2b8791476931aa92f640a436eb4151d3b372027
AlessandroPiazza98 <116750318+AlessandroPiazza98@users.noreply.github.com>
2024-06-22 09:56:26 +0200

    Made coherent for Train_token

--- Models.py
+++ Models.py
@@ -22,19 +22,23 @@ logger.add(sys.stderr, level="INFO")
 
 
 def model_feat_map(input_dim, output_dim, edge_dim, device, hidden, dropout): 
-    dict = {'GCN':GCN(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim).to(device),
+    dict = {'GCN':GCN(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim, hidden=hidden, dropout=dropout).to(device),
+            'GCN_old':GCN_old(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim).to(device),
             'Pool':Pool(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim).to(device),
             'GCN5':GCN5(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim).to(device),
             'SAGPool':SAGPool(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim).to(device),
             'GCN2':GCN2(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim, hidden=hidden, dropout=dropout).to(device),
-            'GCNSAG':GCN2(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim, hidden=hidden, dropout=dropout).to(device)}
+            'GCNSAG':GCN2(in_dim = input_dim, out_dim = output_dim, edge_dim=edge_dim, hidden=hidden, dropout=dropout).to(device),
+            }
     return dict
+
+
 
 def classifier_map(output_dim, device, hidden, dropout, ntoken, nhead, num_encoder_layers, dim_feedforward): 
     dict = {'Classifier':Classifier(output_dim, hidden, dropout).to(device),
             'ClassifierWin':ClassifierWin(output_dim, hidden, ntoken, dropout).to(device),
-            'Transformer':TransformerAndClassifier(d_model= hidden*4, nhead= nhead, num_encoder_layers= num_encoder_layers, dim_feedforward= dim_feedforward, dropout= dropout,
-                                                    max_len= 5000, classifier_layer_size= 64, batch_first=True, classes=output_dim, norm_first=False).to(device)}
+            'Transformer':TransformerAndClassifier(d_model= hidden, nhead= nhead, num_encoder_layers= num_encoder_layers, dim_feedforward= dim_feedforward, dropout= dropout,
+                                                    max_len= 5000, classifier_layer_size= 64, batch_first=True, classes=output_dim, norm_first=False).to(device)} #TODO check for 4x in Transformer
     return dict
 
 def debug_p(string, debug):
@@ -305,7 +309,7 @@ class OldPool(torch.nn.Module):
 
         return x
 
-class GCN(torch.nn.Module):
+class GCN_old(torch.nn.Module):
     def __init__(self, in_dim, out_dim, edge_dim, dropout=0.2):
         super().__init__()
         self.dropout = dropout
@@ -348,7 +352,43 @@ class GCN(torch.nn.Module):
         x = torch.log_softmax(x, dim=-1)
         return x
 
-class GCN2(torch.nn.Module):
+class GCN(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, edge_dim, hidden, dropout):
+        super().__init__()
+        hidden = int(hidden/4)
+        self.dropout = dropout
+        self.conv1 = GENConv(in_dim, hidden, edge_dim=edge_dim)
+        self.norm1 = nn.BatchNorm1d(hidden)
+        self.conv2 = GENConv(hidden, hidden*2, edge_dim=edge_dim)
+        self.norm2 = nn.BatchNorm1d(hidden*2)
+        self.conv3 = GENConv(hidden*2, hidden*4, edge_dim=edge_dim)
+        self.norm3 = nn.BatchNorm1d(hidden*4)
+    
+
+    def forward(self, data, batch, debug=False):
+        
+        x = data.x
+        edge_attr = data.edge_attr
+        edge_index = data.edge_index
+
+        x = self.conv1(x, edge_index, edge_attr)
+        x = self.norm1(x)
+
+        x = nn.LeakyReLU()(x)
+        x = self.conv2(x, edge_index, edge_attr)
+        x = self.norm2(x)
+
+        x = nn.LeakyReLU()(x)
+        x = self.conv3(x, edge_index, edge_attr)
+        x = self.norm3(x)
+
+        x = nn.LeakyReLU()(x)
+
+        x = global_mean_pool(x, batch)       
+        x = F.dropout(x, p=self.dropout)
+        return x
+
+class GCN2(torch.nn.Module): #TODO remove
     def __init__(self, in_dim, out_dim, edge_dim, hidden, dropout):
         super().__init__()
         self.dropout = dropout
@@ -366,26 +406,20 @@ class GCN2(torch.nn.Module):
         edge_attr = data.edge_attr
         edge_index = data.edge_index
 
-        debug_p(f'Start {x.shape}', debug)
         x = self.conv1(x, edge_index, edge_attr)
-        debug_p(f'Conv1 {x.shape}', debug)
         x = self.norm1(x)
 
-        x = x.relu()
+        x = nn.LeakyReLU()(x)
         x = self.conv2(x, edge_index, edge_attr)
-        debug_p(f'Conv2 {x.shape}', debug)
         x = self.norm2(x)
 
-        x = x.relu()
+        x = nn.LeakyReLU()(x)
         x = self.conv3(x, edge_index, edge_attr)
-        debug_p(f'Conv3 {x.shape}', debug)
         x = self.norm3(x)
 
-        x = x.relu()
+        x = nn.LeakyReLU()(x)
 
-        x = global_mean_pool(x, batch)
-        debug_p(f'GMPool {x.shape}', debug)
-       
+        x = global_mean_pool(x, batch)       
         x = F.dropout(x, p=self.dropout)
         return x
 
@@ -452,46 +486,36 @@ class GCN5(torch.nn.Module):
         edge_attr = data.edge_attr
         edge_index = data.edge_index
 
-        debug_p(f'Start {x.shape}', debug)
         x = self.conv1(x, edge_index, edge_attr)
-        debug_p(f'Conv1 {x.shape}', debug)
 
         x = x.relu()
         x = self.conv2(x, edge_index, edge_attr)
-        debug_p(f'Conv2 {x.shape}', debug)
 
         x = x.relu()
         x = self.conv3(x, edge_index, edge_attr)
-        debug_p(f'Conv3 {x.shape}', debug)
 
         x = x.relu()
         x = self.conv4(x, edge_index, edge_attr)
-        debug_p(f'Conv4 {x.shape}', debug)
 
         x = x.relu()
         x = self.conv5(x, edge_index, edge_attr)
-        debug_p(f'Conv5 {x.shape}', debug)
 
         x = x.relu()
         # Perform global mean pooling
         x = global_mean_pool(x, batch)
-        debug_p(f'GMPool {x.shape}', debug)
         
         x = F.dropout(x, p=self.dropout)
         x = self.dense1(x)
-        debug_p(f'Lin1 {x.shape}', debug)
 
         x = self.dense2(x)
-        debug_p(f'Lin2 {x.shape}', debug)
         x = self.dense3(x)
-
-        debug_p(f'Lin3 {x.shape}', debug)
         x = torch.log_softmax(x, dim=-1)
-        return x
+        return x 
 
-class Classifier(torch.nn.Module):
+class Classifier(torch.nn.Module): #TODO There is no RELU and correct the dimensions for hidden
     def __init__(self, out_dim, hidden, dropout):
         super().__init__()
+        hidden = int(hidden/4)
         self.dropout = dropout
         self.dense1 = nn.Linear(hidden*4, hidden*2)
         self.dense2 = nn.Linear(hidden*2, hidden)
@@ -500,17 +524,11 @@ class Classifier(torch.nn.Module):
     def forward(self, x, debug=False):
         x = x
 
-        debug_p(f'Start {x.shape}', debug)
         x = self.dense1(x)
-        debug_p(f'Lin1 {x.shape}', debug)
         x = F.dropout(x, p=self.dropout)
-
         x = self.dense2(x)
-        debug_p(f'Lin2 {x.shape}', debug)
         x = F.dropout(x, p=self.dropout)
-
         x = self.dense3(x)
-        debug_p(f'Lin3 {x.shape}', debug)
 
         x = torch.log_softmax(x, dim=-1)
         return x    
@@ -519,6 +537,7 @@ class Classifier(torch.nn.Module):
 class ClassifierWin(torch.nn.Module):
     def __init__(self, out_dim, hidden, tokens, dropout):
         super().__init__()
+        hidden=int(hidden/4)
         self.dropout = dropout
         self.dense1 = nn.Linear(hidden*4*tokens, hidden*2*tokens)
         self.dense2 = nn.Linear(hidden*2*tokens, hidden*tokens)
@@ -527,17 +546,11 @@ class ClassifierWin(torch.nn.Module):
     def forward(self, x, debug=False):
         x = x
 
-        debug_p(f'Start {x.shape}', debug)
         x = self.dense1(x)
-        debug_p(f'Lin1 {x.shape}', debug)
         x = F.dropout(x, p=self.dropout)
-
         x = self.dense2(x)
-        debug_p(f'Lin2 {x.shape}', debug)
         x = F.dropout(x, p=self.dropout)
-
         x = self.dense3(x)
-        debug_p(f'Lin3 {x.shape}', debug)
 
         x = torch.log_softmax(x, dim=-1)
         return x    
@@ -552,7 +565,7 @@ class TransformerAndClassifier(nn.Module):
                                        max_len, batch_first, norm_first)
         self.classifier = ClassifierHead(d_model, classes, dropout=dropout, linear_size=classifier_layer_size)
 
-    def forward(self, x, debug=False):
+    def forward(self, x):
         cls_token = self.transformer(x)
         x = self.classifier(cls_token)
         return x
@@ -710,3 +723,74 @@ class ClassifierHead(nn.Module):
         x = self.layers(x)
         logger.debug(f"output in classifier head {x.shape}")
         return x
+    
+
+# Define the Encoder (aligned with GCN2 model)
+class GraphEncoder(nn.Module):
+    def __init__(self, in_dim, hidden, edge_dim, out_dim, dropout):
+        super(GraphEncoder, self).__init__()
+        self.dropout = dropout
+        self.conv1 = GENConv(in_dim, hidden, edge_dim=edge_dim)
+        self.norm1 = nn.BatchNorm1d(hidden)
+        self.conv2 = GENConv(hidden, hidden, edge_dim=edge_dim)
+        self.norm2 = nn.BatchNorm1d(hidden)
+        self.conv3 = GENConv(hidden, hidden, edge_dim=edge_dim)
+        self.norm3 = nn.BatchNorm1d(hidden)
+        self.lin = nn.Linear(hidden, out_dim)
+
+    def forward(self, data):
+        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+
+        x = self.conv1(x, edge_index, edge_attr)
+        x = self.norm1(x)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index, edge_attr)
+        x = self.norm2(x)
+        x = F.relu(x)
+        x = self.conv3(x, edge_index, edge_attr)
+        x = self.norm3(x)
+        x = F.relu(x)
+
+        x = global_mean_pool(x, data.batch)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.lin(x)
+        return x
+
+# Define the Decoder
+class GraphDecoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, edge_dim, dropout):
+        super(GraphDecoder, self).__init__()
+        self.dropout = dropout
+        self.lin = nn.Linear(input_dim, hidden_dim)
+        self.conv1 = GENConv(hidden_dim, hidden_dim, edge_dim=edge_dim)
+        self.norm1 = nn.BatchNorm1d(hidden_dim)
+        self.conv2 = GENConv(hidden_dim, hidden_dim, edge_dim=edge_dim)
+        self.norm2 = nn.BatchNorm1d(hidden_dim)
+        self.conv3 = GENConv(hidden_dim, output_dim, edge_dim=edge_dim)
+        self.norm3 = nn.BatchNorm1d(output_dim)
+
+    def forward(self, z, data):
+        z = self.lin(z)
+        z = F.relu(z)
+        z = z[data.batch] #TODO
+        x = self.conv1(z, data.edge_index, data.edge_attr)
+        x = self.norm1(x)
+        x = F.relu(x)
+        x = self.conv2(x, data.edge_index, data.edge_attr)
+        x = self.norm2(x)
+        x = F.relu(x)
+        x = self.conv3(x, data.edge_index, data.edge_attr)
+        x = self.norm3(x)
+        return x
+
+# Define the Autoencoder
+class GraphAutoencoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(GraphAutoencoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, data):
+        z = self.encoder(data)
+        recon_x = self.decoder(z, data)
+        return recon_x
